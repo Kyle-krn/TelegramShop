080741459fb39260c0351a8925187b729f87aafd
andrew <vesell4123@mail.ru>
2022-11-27 13:33:08 +0300

    hide data.config in env

--- data/config.py
+++ data/config.py
@@ -4,28 +4,28 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = env.str("BOT_TOKEN") # Забираем значение типа str
+ADMINS = env.list("ADMINS") # Тут у нас будет список из админов
+# IP = env.str("ip") # Тоже str, но для айпи адреса хоста
 API_URL = env.str("API_URL")
 API_TOKEN = env.str("API_TOKEN")
 PAYMENTS_PROVIDER_TOKEN = env.str("PAYMENTS_PROVIDER_TOKEN")
-USER = 'telegram_user'
-PASSWORD = 'remdigga96'
-HOST = '0.0.0.0'
-PORT = 5432
-DATABASE = 'telegram_shop'
+USER = env.str("USER")
+PASSWORD = env.str("PASSWORD")
+HOST = env.str("HOST")
+PORT = env.int("PORT")
+DATABASE = env.str("DATABASE")
 
 POSTGRES_URI = f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 TORTOISE_ORM = {
-    "connections": {"default": POSTGRES_URI},
-    "apps": {
-        "models": {
-            "models": ["models.models", "aerich.models"],
-            "default_connection": "default",
-        },
-    },
-    "use_tz": False,
-    "timezone": "UTC"
+"connections": {"default": POSTGRES_URI},
+"apps": {
+"models": {
+"models": ["models.models", "aerich.models"],
+"default_connection": "default",
+},
+},
+"use_tz": False,
+"timezone": "UTC",
 }
