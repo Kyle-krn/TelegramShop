e890f72d68fe1972de2b7dd7653e0c02d91f591f
andrew <vesell4123@mail.ru>
2022-11-27 13:01:56 +0300

    hide data in env

--- data/config.py
+++ data/config.py
@@ -10,11 +10,11 @@ ADMINS = env.list("ADMINS")  # Тут у нас будет список из а�
 API_URL = env.str("API_URL")
 API_TOKEN = env.str("API_TOKEN")
 PAYMENTS_PROVIDER_TOKEN = env.str("PAYMENTS_PROVIDER_TOKEN")
-USER = ""
-PASSWORD = ""
-HOST = ""
+USER = "vfpflwrv"
+PASSWORD = "ooDYh8eT9H9fOfAo3sP7HeSl46orHyfC"
+HOST = "abul.db.elephantsql.com"
 PORT = 5432
-DATABASE = ""
+DATABASE = "vfpflwrv"
 
 POSTGRES_URI = f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
